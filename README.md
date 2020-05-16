3eee9645e9e7d84d4f36726e580c649c7942901e
Matheus Fernando Sfalsin Mathias <matheussfalsin@gmail.com>
2020-05-16 18:30:13 -0300

    Atualização README

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# Organize-se
+<h1 align="center">
+Organize-se
+<br>
+</h1>
+
+<h3 align="center">
 Projeto de Organização pessoal em formato de Listas
+<br>
+</h3>
+<br>
+
+- O que é o site?
+
+  > Aplicativo desenvolvido afim de ajudar na listagem de elementos do dia a dia. Exemplo a listagem itens para compras de supermercado, listagem de atividades a serem feitas, entre outras
+
+- Telas do aplicativo:
+
+  ### :camera: Tela de Login
+
+  ![png1](github/Login.jpg)
+
+  ### :camera: Cadastro do Usuário
+
+  ![png2](github/Cadastro.jpg)
+
+  ### :camera: Listagem
+
+  ![png2](github/Listas.jpg) ![png2](github/Criacao_lista.jpg)
+
+  ### :camera: Detalhes de uma Lista
+
+  ![png2](github/Detalhes_lista.jpg) ![png2](github/Criacao_Detalhes_lista.jpg)
+
+  ## Tecnologias:
+
+  - [x] React Native
+  - [x] Firebase(Autenticação, Banco de dados)
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,42 +1,37 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 // import { View } from 'react-native';
 
 const appStack = createStackNavigator();
 
-import Login from './pages/Login/index'
-import Register from './pages/Register/index'
-import List from './pages/List/index'
-import SubList from './pages/SubList/index'
-
-import auth from '@react-native-firebase/auth'
+import Login from './pages/Login/index';
+import Register from './pages/Register/index';
+import List from './pages/List/index';
+import SubList from './pages/SubList/index';
+import auth from '@react-native-firebase/auth';
 
 export default function Routes() {
-    return (// tem que vir por volta das rotas
-        <NavigationContainer>
-            <appStack.Navigator screenOptions={{ headerShown: false }}>
-
-                {
-                    auth().currentUser ?
-                        <>
-                            <appStack.Screen name="List" component={List} />
-                            <appStack.Screen name="SubList" component={SubList} />
-                            <appStack.Screen name="Login" component={Login} />
-                            <appStack.Screen name="Register" component={Register} />
-
-                        </>
-                        :
-                        <>
-                            <appStack.Screen name="Login" component={Login} />
-                            <appStack.Screen name="Register" component={Register} />
-                            <appStack.Screen name="List" component={List} />
-                            <appStack.Screen name="SubList" component={SubList} />
-                        </>
-                }
-
-
-            </appStack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    // tem que vir por volta das rotas
+    <NavigationContainer>
+      <appStack.Navigator screenOptions={{headerShown: false}}>
+        {auth().currentUser ? (
+          <>
+            <appStack.Screen name="List" component={List} />
+            <appStack.Screen name="SubList" component={SubList} />
+            <appStack.Screen name="Login" component={Login} />
+            <appStack.Screen name="Register" component={Register} />
+          </>
+        ) : (
+          <>
+            <appStack.Screen name="Login" component={Login} />
+            <appStack.Screen name="Register" component={Register} />
+            <appStack.Screen name="List" component={List} />
+            <appStack.Screen name="SubList" component={SubList} />
+          </>
+        )}
+      </appStack.Navigator>
+    </NavigationContainer>
+  );
 }
